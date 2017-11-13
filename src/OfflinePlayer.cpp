c5dc1e3e5d77a504a46e8db27585828e94a5b511
Tal Pogorelis <Talpogorelis@gmail.com>
2017-11-13 13:36:47 +0200

    input example

--- src/OfflinePlayer.cpp
+++ src/OfflinePlayer.cpp
@@ -6,6 +6,7 @@
 
 #include "OfflinePlayer.h"
 #include <iostream>
+#include <limits>
 
 /**
  * The constructor method.
@@ -20,8 +21,10 @@ OfflinePlayer::OfflinePlayer(int color): Player(color){
  * The function lets the player choose his next move.
  * @return - a Cell containing the X,Y values of the next move.
  */
-Cell OfflinePlayer::chooseMove(){
-	std::cout << "Please enter your move row,col: ";
+Cell OfflinePlayer::chooseMove() {
+
+	std::cout << "Please enter your move row col (for example 3 5): ";
+	bool validInput = false;
 	int x, y;
 	std::cin >> x >> y;
 	Cell chosenXY = Cell(x,y);
