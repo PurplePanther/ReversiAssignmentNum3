1f4e933c038d3b7d56b801ab143b6f412938f179
NatalieBM6 <33790319+NatalieBM6@users.noreply.github.com>
2017-11-28 02:55:27 +0200

    Adding tests for the menu.

--- UnitTests/test/test_Menu.cpp
+++ UnitTests/test/test_Menu.cpp
@@ -5,3 +5,25 @@
 #include "gtest/gtest.h"
 #include "Menu.h"
 
+//Testing the getGameMode method.
+TEST(MenuTest,checkGetGameMode){
+	Menu test=Menu();
+	if((test.getGameMode() ==1) ||(test.getGameMode() ==2)){
+		ASSERT_TRUE(true);
+	}
+	else{
+		ASSERT_TRUE(false);
+	}
+}
+//Testing the chooseGameMode method.
+TEST(MenuTest,checkChooseGameMode){
+	Menu test=Menu();
+	test.chooseGameMode();
+	if((test.getGameMode() ==1) ||(test.getGameMode() ==2)){
+		ASSERT_TRUE(true);
+	}
+	else{
+		ASSERT_TRUE(false);
+	}
+}
+
